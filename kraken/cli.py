69bffc3a5e20486efe8ea82086ae2510655fa84b
Taylor McKinnon <mail@tmacs.space>
2020-01-03 12:18:24 -0800

    Add more cli flags

--- kraken/cli.py
+++ kraken/cli.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 import sys
-
+import pathlib
 
 def cloud_target(opt):
     if opt not in ['blob', 's3']:
@@ -18,6 +18,9 @@ def int_or_none(opt):
         return opt
     return int(opt)
 
+def path(opt):
+    return pathlib.PosixPath(opt).resolve()
+
 def get_args():
     parser = argparse.ArgumentParser(
 		prog=os.path.basename(sys.argv[0]),
@@ -27,5 +30,8 @@ def get_args():
     parser.add_argument('-s', '--size', action='store', type=int, default=1024, help='Specify the file size in bytes for put workloads.')
     parser.add_argument('-d', '--duration', action='store', type=int, default=60, help='Load duration in seconds.')
     parser.add_argument('-a', '--action', action='store', type=cloud_op, default='put', help='Operation to perform on the target. (put/get)')
+    parser.add_argument('-b', '--bucket', action='store', default='testbucket', help='Specify the bucket/container to use for workloads.')
+    parser.add_argument('-k', '--key-prefix', action='store', default='object', help='Specify the prefix to use for keys during uploads.')
+    parser.add_argument('-o', '--output', action='store', required=True, type=path, help='Specifiy the path to use for results data.')
     parser.add_argument('--cs', '--connection-string', action='store', dest='connect_str', default=None, help='Specify the Azure connction string for blob targets. If not provided it is read from the env var `AZURE_STORAGE_CONNECTION_STRING`')
     return parser.parse_args()
